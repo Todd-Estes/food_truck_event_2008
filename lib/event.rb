869e3f4de558ef6c3c04bf02339c575ea8662108
todd-estes <elestes@gmail.com>
2020-09-15 14:08:14 -0600

    Add event.rb

---
+++ lib/event.rb
@@ -0,0 +1,18 @@
+class Event
+  attr_reader :name,  :food_trucks
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(food_truck_obj)
+    @food_trucks << food_truck_obj
+  end
+
+  def food_truck_names
+    @food_trucks.map do |truck|
+      truck.name
+    end
+  end
+
+end
